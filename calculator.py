440f9dfe936d2cb9c494bd07cee7ff1207590cb1
SagarR.B <sagarrb1805@gmail.com>
2021-02-13 20:28:28 +0530

    added the display label part

---
+++ calculator.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('500x600')
+root.title("Calculator")
+root.config(bg='grey')
+
+display_label = Label(root, width=65, height=8)
+display_label.place(x=20, y=15)
+
+
+
+
+
+
+root.mainloop()
